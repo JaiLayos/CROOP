13bc3d134676fa4736c6b3c2aa68a9b66192bc07
JAI RUS MATTHEW LAYOS <jrmdlayos@mymail.mapua.edu.ph>
2025-03-30 17:34:13 +0800

    check if ordered and arrange notifications

--- src/main/java/com/jai/croop/repository/NotificationsRepository.java
+++ src/main/java/com/jai/croop/repository/NotificationsRepository.java
@@ -2,9 +2,13 @@ package com.jai.croop.repository;
 
 import com.jai.croop.model.Notifications;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NotificationsRepository extends JpaRepository<Notifications, Integer> {
-    List<Notifications> findByUserIDAndUserType(int userID, String userType);
+    @Query("SELECT n FROM Notifications n WHERE n.userID = :userID AND n.userType = :userType ORDER BY n.date DESC")
+    List<Notifications> findByUserIDAndUserType(@Param("userID") int userID, @Param("userType") String userType);
+
 }
